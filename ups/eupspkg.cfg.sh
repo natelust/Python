fed5ae191b41761d5144e77fb7a71246e533d3b5
Mario Juric <mjuric@lsst.org>
2013-12-12 12:24:40 -0700

    Initial implementation of python version checker stub package

---
+++ ups/eupspkg.cfg.sh
@@ -0,0 +1,12 @@
+config()
+{
+	# Verify that a compatible module can be found exists
+	./scripts/check_python -v | eups_console
+
+	if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+		die "Failed to find a compatible externally provided Python 2.7."
+	fi
+}
+
+prep() { :; }
+build() { :; }
