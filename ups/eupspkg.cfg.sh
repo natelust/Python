36ca01c0ca941729a644384119ed96b25130e519
Tim Jenness <tjenness@lsst.org>
2016-06-01 09:29:24 -0700

    Allow python 3 to pass test
    Uses bc command for floating point comparison.
    
    Checks that minimum python3 version is 3.4 and minimum python2
    version is 2.7.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -4,7 +4,7 @@ config()
 	./scripts/check_python -v | eups_console
 
 	if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-		die "Failed to find a compatible externally provided Python 2.7."
+		die "Failed to find a compatible externally provided Python."
 	fi
 }
 
